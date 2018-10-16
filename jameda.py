dca68fbbf2b21fadb5bc6056ecb31816e5efcf72
jahidmixmaster <jahid6847@gmail.com>
2018-10-16 18:09:54 +0200

    fix crawler duplicate error

--- jameda.py
+++ jameda.py
@@ -5,11 +5,15 @@ import scrapy
 class JamedaSpider(scrapy.Spider):
     name = "jameda"
     start_urls = [
-        'https://www.jameda.de/berlin/aerzte/urologen/dr-christian-klopf/uebersicht/81098175_1/',
+        'https://www.jameda.de/berlin/aerzte/augenaerzte/dr-kirk-nordwald/uebersicht/80121093_1/',
     ]
 
     headers = {'User-Agent' : 'Mozilla/5.0'}
     base_url = 'https://www.jameda.de'
+    boffset_100 = '?boffset=100'
+    boffset_200 = '&boffset=200'
+    boffset_300 = '&boffset=300'
+    boffset_400 = '&boffset=400'
 
     def start_requests(self):
         for url,tag in zip(self.start_urls, range(len(self.start_urls))):
@@ -32,6 +36,7 @@ class JamedaSpider(scrapy.Spider):
             'Gesetzlich-Versicherte': response.xpath('//*[@id="content"]/div[2]/div[2]/div[2]/div[1]/div[1]/div/text()').extract_first(),
             'Privatversicherte': response.xpath('//*[@id="content"]/div[2]/div[2]/div[2]/div[1]/div[2]/div/div/text()').extract_first(),
             'Anzahl-Bewertungen': response.xpath('//*[@id="profillasche_note"]/div[1]/span/text()').extract_first(),
+            'meta': response.xpath('//*[@id="profillasche_note"]/div[1]/span/text()').extract_first(),
             'reviews' : list()
         }
 
@@ -39,43 +44,65 @@ class JamedaSpider(scrapy.Spider):
         
         request = scrapy.Request(url=reviewsLink, headers=self.headers, callback=self.parse_reviews)
         request.meta['item'] = item
+        request.meta['reviewsLink'] = reviewsLink
         return request
 
     def parse_reviews(self, response):
         item = response.meta['item']
+        
+        reviewsLink = response.meta['reviewsLink']
+        
         reviews = list()
         temp = len(response.css('div.bewertung').extract())
         
         
         single_ratings = response.css('table.table.gesamtbewertung')
         for i in range(0,temp):
-            single_rating = dict()
-            temp1 = len(single_ratings[i].css('div.minoTooltip.fragezeichen_cursor::text').extract())
-            for j in range(0,temp1):
-                single_rating[single_ratings[i].css('div.minoTooltip.fragezeichen_cursor::text').extract()[j]] =                                                                                   single_ratings[i].css('.note-small::text').extract()[j]
-            review = {
-                'BName': response.css('h2 > a::text').extract()[i],
-                'BNote': response.css('div.note1::text').extract()[i],
-                'BMeta-Info': response.css('div.text > p.text-klein::text').extract()[i],
-                'BText': response.css('div.fliesstext::text').extract()[i+1] + response.css('div.fliesstext::text').extract()[i+2],
-                'BEinzelne-Bewertung' : single_rating
-            }
-            reviews.append(review)
+            try:
+                single_rating = dict()
+                temp1 = len(single_ratings[i].css('div.minoTooltip.fragezeichen_cursor::text').extract())
+                for j in range(0,temp1):
+                    single_rating[single_ratings[i].css('div.minoTooltip.fragezeichen_cursor::text').extract()[j]] =                                                                                   single_ratings[i].css('.note-small::text').extract()[j]
+                review = {
+                    'BName': response.css('h2 > a::text').extract()[i],
+                    'BNote': response.css('div.note1::text').extract()[i],
+                    'BMeta-Info': response.css('div.text > p.text-klein::text').extract()[i],
+                    'BText': response.css('div.fliesstext::text').extract()[i+1] + response.css('div.fliesstext::text').extract()[i+2],
+                    'BEinzelne-Bewertung' : single_rating
+                }
+                reviews.append(review)
+            except:
+                pass
             
         item['reviews'] = item['reviews'] + reviews
 
-        if(int(item['Anzahl-Bewertungen']) <= 100):
-            
+        if(int(item['meta']) <= 100):
+            # only for test
+            file = open('test.txt','w') 
+            file.write(str(len(item['reviews']))) 
+            file.close() 
             yield item
-        else:
-            half_link = response.xpath('//*[@id="content"]/div[1]/div[1]/div[{0}]/a[1]/@href'.format(3)).extract_first()
-            if(isinstance(half_link, str)):
-                reviewsLink = self.base_url + half_link
-                yield scrapy.Request(url=reviewsLink, meta={'item': item}, headers=self.headers, callback=self.parse_reviews)
-            else:
-                half_link = response.xpath('//*[@id="content"]/div[1]/div[1]/div[{0}]/a[1]/@href'.format(5)).extract_first()
-                reviewsLink = self.base_url + half_link
-                yield scrapy.Request(url=reviewsLink, meta={'item': item}, headers=self.headers, callback=self.more_reviews)
+        elif(int(item['meta']) > 100 and int(item['meta']) < 200):
+            temp = int(item['meta']) - 100
+            item['meta'] = str(temp)
+            url=reviewsLink + self.boffset_100
+            yield scrapy.Request(url=url, meta={'item': item,'reviewsLink': reviewsLink}, headers=self.headers, callback=self.parse_reviews)
+        elif(int(item['meta']) > 200 and int(item['meta']) < 300):
+            temp = int(item['meta']) - 100
+            item['meta'] = str(temp)
+            url=reviewsLink + self.boffset_100 + self.boffset_200
+            yield scrapy.Request(url=url, meta={'item': item,'reviewsLink': reviewsLink}, headers=self.headers, callback=self.parse_reviews)
+        elif(int(item['meta']) > 300 and int(item['meta']) < 400):
+            temp = int(item['meta']) - 100
+            item['meta'] = str(temp)
+            url=reviewsLink + self.boffset_100 + self.boffset_200 + self.boffset_300
+            yield scrapy.Request(url=url, meta={'item': item,'reviewsLink': reviewsLink}, headers=self.headers, callback=self.parse_reviews)
+        elif(int(item['meta']) > 400 and int(item['meta']) < 500):
+            temp = int(item['meta']) - 100
+            item['meta'] = str(temp)
+            url=reviewsLink + self.boffset_100 + self.boffset_200 + self.boffset_300 + self.boffset_400
+            yield scrapy.Request(url=url, meta={'item': item,'reviewsLink': reviewsLink}, headers=self.headers, callback=self.parse_reviews)
+            
 
     def more_reviews(self,response):
         item = response.meta['item']
@@ -84,18 +111,21 @@ class JamedaSpider(scrapy.Spider):
 
         single_ratings = response.css('table.table.gesamtbewertung')
         for i in range(0,temp):
-            single_rating = dict()
-            temp1 = len(single_ratings[i].css('div.minoTooltip.fragezeichen_cursor::text').extract())
-            for j in range(0,temp1):
-                single_rating[single_ratings[i].css('div.minoTooltip.fragezeichen_cursor::text').extract()[j]] =                                                                                   single_ratings[i].css('.note-small::text').extract()[j]
-            review = {
-                'BName': response.css('h2 > a::text').extract()[i],
-                'BNote': response.css('div.note1::text').extract()[i],
-                'BMeta-Info': response.css('div.text > p.text-klein::text').extract()[i],
-                'BText': response.css('div.fliesstext::text').extract()[i+1] + response.css('div.fliesstext::text').extract()[i+2],
-                'BEinzelne-Bewertung' : single_rating
-            }
-            reviews.append(review)
+            try:
+                single_rating = dict()
+                temp1 = len(single_ratings[i].css('div.minoTooltip.fragezeichen_cursor::text').extract())
+                for j in range(0,temp1):
+                    single_rating[single_ratings[i].css('div.minoTooltip.fragezeichen_cursor::text').extract()[j]] =                                                                                   single_ratings[i].css('.note-small::text').extract()[j]
+                review = {
+                    'BName': response.css('h2 > a::text').extract()[i],
+                    'BNote': response.css('div.note1::text').extract()[i],
+                    'BMeta-Info': response.css('div.text > p.text-klein::text').extract()[i],
+                    'BText': response.css('div.fliesstext::text').extract()[i+1] + response.css('div.fliesstext::text').extract()[i+2],
+                    'BEinzelne-Bewertung' : single_rating
+                }
+                reviews.append(review)
+            except:
+                pass
 
         item['reviews'] = item['reviews'] + reviews
         
